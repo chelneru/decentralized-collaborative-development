49a03a2b92a96681cabbe78ebda82ea41e697b41
Alin Panainte <au614698@uni.au.dk>
2020-02-02 15:48:55 +0100

    initial commit

---
+++ ConsoleApp1/Peer.cs
@@ -0,0 +1,49 @@
+﻿using Ipfs.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Peer
+    {
+        public readonly IpfsClient ipfs;
+        private string repoPath;
+        private Ipfs.Peer ipfsPeer;
+        public Peer()
+        {
+            this.ipfs = new IpfsClient();
+            //ipfsPeer = await ipfs.IdAsync();
+            //TODO
+
+        }
+
+        public async void InitiateIPFS(Boolean isBootstrap)
+        {
+            ipfsPeer = await ipfs.IdAsync();
+        }
+
+        public async System.Threading.Tasks.Task<IEnumerable<Ipfs.Peer>> GetConnectedPeers()
+        {
+            //TODO
+            return await ipfs.Swarm.PeersAsync();
+        }
+        public async System.Threading.Tasks.Task<Ipfs.IFileSystemNode> BroadcastFileData(string path)
+        {
+            //TODO
+            return await ipfs.FileSystem.AddFileAsync(path);
+
+        }
+        public async System.Threading.Tasks.Task<Ipfs.IFileSystemNode> BroadcastStream(System.IO.Stream stream)
+        {
+            //TODO
+            return await ipfs.FileSystem.AddAsync(stream);
+
+        }
+        ~Peer()
+        {
+            //TODO
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ipfs.Http;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static readonly IpfsClient ipfs = new IpfsClient();
+        Peer newPeer = new Peer();
+        
+        
+        //Helpers.InitializeIpfsAsync();
+
+    }
+}
